10e873dea6fd63f174c857f628e298d789ac2ac9
KyujinPark01 <rbwls1100@naver.com>
2025-12-09 11:01:32 +0900

    feat : sidebar 추가

---
+++ src/components/layout/sidebar/Sidebar.vue
@@ -0,0 +1,120 @@
+<template>
+  <aside class="sidebar-wrapper">
+
+    <!-- 타이머 박스 -->
+    <div class="card-panel timer-panel">
+      <div class="timer-text">{{ timer }}</div>
+
+      <div class="button-row">
+        <CommonButton buttonClass="btn-green">시작</CommonButton>
+        <CommonButton buttonClass="btn-red">종료</CommonButton>
+      </div>
+    </div>
+
+    <!-- 잔고 -->
+    <div>
+      <p class="section-title-bold">잔고</p>
+      <div class="card-panel balance-panel">
+        $10,000
+      </div>
+    </div>
+
+    <!-- 보유 현황 -->
+    <div>
+      <p class="section-title-bold">보유 현황</p>
+
+      <div class="card-panel holding-panel">
+
+        <!-- 헤더 -->
+        <div class="holding-header">
+          <span>종목명</span>
+          <span>수량</span>
+          <span>평균단가</span>
+        </div>
+
+        <!-- 데이터 -->
+        <div
+            v-for="item in holdings"
+            :key="item.id"
+            class="holding-row"
+        >
+          <div class="holding-left">
+            <p class="holding-name">{{ item.name }}</p>
+            <p class="holding-ticker">{{ item.ticker }}</p>
+          </div>
+
+          <p class="holding-mid">{{ item.quantity }}</p>
+
+          <p class="holding-price-final">$ {{ item.avgPrice }}</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- 순위 -->
+    <div>
+      <p class="section-title-bold">현재 순위</p>
+
+      <div class="card-panel ranking-panel">
+
+        <!-- 상위 3명 -->
+        <div
+            v-for="rank in ranking.slice(0, 3)"
+            :key="rank.rank"
+            class="rank-item"
+        >
+          <div class="rank-left">
+            <span v-if="rank.rank === 1">🥇</span>
+            <span v-else-if="rank.rank === 2">🥈</span>
+            <span v-else-if="rank.rank === 3">🥉</span>
+            <span>{{ rank.nickname }}</span>
+          </div>
+
+          <div class="rank-money">
+            $ {{ rank.total }}
+          </div>
+        </div>
+
+        <div class="rank-dots">•••</div>
+
+        <!-- 나머지 순위 -->
+        <div
+            v-for="rank in ranking.slice(3)"
+            :key="rank.rank"
+            class="rank-item rank-low"
+        >
+          <p>{{ rank.rank }}등 {{ rank.nickname }}</p>
+          <p>$ {{ rank.total }}</p>
+        </div>
+
+      </div>
+    </div>
+
+  </aside>
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+import '@/assets/sidebar/sidebar.css'
+import CommonButton from '@/components/common/button/CommonButton.vue'
+const timer = ref("00 : 10 : 00")
+
+const holdings = ref([
+  { id: 1, name: "엔비디아", ticker: "NVDA", quantity: 20, avgPrice: "172.80" },
+  { id: 2, name: "엔비디아", ticker: "NVDA", quantity: 20, avgPrice: "172.80" }
+])
+
+const ranking = ref([
+  { rank: 1, nickname: "최지원", total: "11,350,000" },
+  { rank: 2, nickname: "박규진", total: "11,340,000" },
+  { rank: 3, nickname: "김진", total: "9,340,000" },
+  { rank: 455, nickname: "강성현", total: "340,000" },
+  { rank: 456, nickname: "정동욱", total: "240,000" },
+  { rank: 457, nickname: "야무께", total: "140,000" },
+])
+</script>
+
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/auth/LoginView.vue'
 import RegisterView from '../views/auth/RegisterView.vue'
 import FindPasswordView from '../views/auth/FindPasswordView.vue'
+import Sidebar from "@/components/layout/sidebar/Sidebar.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,11 @@ const router = createRouter({
       name: 'find-password',
       component: FindPasswordView,
     },
+      {
+          path: '/sidebar',
+          name: 'sidebar',
+          component: Sidebar,
+      },
   ],
 })
 
